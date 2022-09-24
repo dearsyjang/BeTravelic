794f368d5788e7b19352a6c40981221420e05ba1
YOONHAE <ue0828@naver.com>
2022-09-24 19:50:45 +0900

    [feat] setting signup S07P22D205-202

--- Back/demo/src/main/java/beTravelic/demo/domain/controller/UserController.java
+++ Back/demo/src/main/java/beTravelic/demo/domain/controller/UserController.java
@@ -1,46 +1,46 @@
-//package beTravelic.demo.domain.controller;
-//
-//import beTravelic.demo.domain.dto.LoginRequestDto;
-//import beTravelic.demo.domain.dto.SignUpRequestDto;
-//import beTravelic.demo.domain.service.UserService;
-//import beTravelic.demo.global.common.CommonResponse;
-//import io.swagger.annotations.ApiOperation;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.validation.annotation.Validated;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import java.io.IOException;
-//
-//@RestController
-//@RequestMapping("/users")
-//@RequiredArgsConstructor
-//public class UserController {
-//    private final UserService userService;
-//    @PostMapping
-//    @ApiOperation(value = "회원가입", notes = "id, pw 입력")
-//    public ResponseEntity<CommonResponse> signUpUser(@ModelAttribute @Validated SignUpRequestDto dto) throws IOException {
-//        return new ResponseEntity<>(CommonResponse.getSuccessResponse(userService.signUpUser(dto)), HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/login")
-//    @ApiOperation(value = "로그인", notes = "id, pw 입력")
-//    public ResponseEntity<CommonResponse> login(@Validated LoginRequestDto dto) throws Exception {
-//        return new ResponseEntity<>(CommonResponse.getSuccessResponse(userService.login(dto)), HttpStatus.OK);
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<CommonResponse> getUserInfo(HttpServletRequest request){
-//        String id = (String) request.getAttribute("id");
-//        return new ResponseEntity<>(CommonResponse.getSuccessResponse(userService.getUserInfo(id)), HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/nickname/{nickName}")
-//    @ApiOperation(value = "닉네임 중복 확인", notes = "닉네임 입력")
-//    public ResponseEntity<?> checkNickname(@PathVariable("nickName") String nickName){
-//        userService.checkNickname(nickName);
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
-//}
+package beTravelic.demo.domain.controller;
+
+import beTravelic.demo.domain.dto.LoginRequestDto;
+import beTravelic.demo.domain.dto.SignUpRequestDto;
+import beTravelic.demo.domain.service.UserService;
+import beTravelic.demo.global.common.CommonResponse;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    @PostMapping
+    @ApiOperation(value = "회원가입", notes = "id, pw 입력")
+    public ResponseEntity<CommonResponse> signUpUser(@ModelAttribute @Validated SignUpRequestDto dto) throws IOException {
+        return new ResponseEntity<>(CommonResponse.getSuccessResponse(userService.signUpUser(dto)), HttpStatus.OK);
+    }
+
+    @GetMapping("/login")
+    @ApiOperation(value = "로그인", notes = "id, pw 입력")
+    public ResponseEntity<CommonResponse> login(@Validated LoginRequestDto dto) throws Exception {
+        return new ResponseEntity<>(CommonResponse.getSuccessResponse(userService.login(dto)), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<CommonResponse> getUserInfo(HttpServletRequest request){
+        String id = (String) request.getAttribute("id");
+        return new ResponseEntity<>(CommonResponse.getSuccessResponse(userService.getUserInfo(id)), HttpStatus.OK);
+    }
+
+    @GetMapping("/nickname/{nickName}")
+    @ApiOperation(value = "닉네임 중복 확인", notes = "닉네임 입력")
+    public ResponseEntity<?> checkNickname(@PathVariable("nickName") String nickName){
+        userService.checkNickname(nickName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
--- Back/demo/src/main/java/beTravelic/demo/domain/dto/UserInfoResponseDto.java
+++ Back/demo/src/main/java/beTravelic/demo/domain/dto/UserInfoResponseDto.java
@@ -1,29 +1,31 @@
-//package beTravelic.demo.domain.dto;
-//
-//import beTravelic.demo.domain.entity.User;
-//import com.fasterxml.jackson.annotation.JsonInclude;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@JsonInclude(value = JsonInclude.Include.NON_NULL)
-//@Builder
-//public class UserInfoResponseDto {
-//    private Long userSeq;
-//    private String id;
-//    private String pw;
-//    private String nickname;
-//
-//    public static UserInfoResponseDto ofUser(User user){
-//        return UserInfoResponseDto.builder()
-//                .userSeq(user.getUserSeq())
-//                .id(user.getId())
-//                .pw(user.getPw())
-//                .nickname(user.getNickName())
-//                .build();
-//    }
-//}
+package beTravelic.demo.domain.dto;
+
+import beTravelic.demo.domain.entity.User;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+@Builder
+public class UserInfoResponseDto {
+    private Long user_id;
+    private String id;
+    private String pw;
+    private String nickname;
+    private String email;
+
+    public static UserInfoResponseDto ofUser(User user){
+        return UserInfoResponseDto.builder()
+                .user_id(user.getUser_id())
+                .id(user.getId())
+                .pw(user.getPw())
+                .nickname(user.getNickname())
+                .email(user.getEmail())
+                .build();
+    }
+}
