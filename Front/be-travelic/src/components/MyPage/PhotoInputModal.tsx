73a0220a5f1dab6a37d3e2a8d4d5b598cf89b2de
신혜원 <hehe2you@gmail.com>
2022-09-26 10:10:30 +0900

    [feat] : 마이페이지 map 클릭 시 사진 업로드로 수정

--- Front/be-travelic/src/components/MyPage/PhotoInputModal.tsx
+++ Front/be-travelic/src/components/MyPage/PhotoInputModal.tsx
@@ -10,28 +10,55 @@ const FILE_SIZE_MAX_LIMIT = 5 * 1024 * 1024; // 5MB
 
 const PhotoInputModal: React.FC<{
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
-  setShowArticleModal: React.Dispatch<React.SetStateAction<boolean>>;
+  // setShowArticleModal: React.Dispatch<React.SetStateAction<boolean>>;
 }> = (props) => {
   const imageInput = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File>();
+  const [image, setImage] = useState<string>();
+
   const uploadImageHandler = () => {
     imageInput.current?.click();
   };
 
+
+  // image에 
   const changePhotoHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.files![0]);
+    if (event.target.files![0]) {
+      setFile(event.target.files![0]);
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2 && typeof reader.result === "string") {
+        setImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(event.target.files![0]);
   };
 
   const modalCloseHandler = () => {
     props.setShowModal(false);
   };
 
-  const showArticleModalHandler = () => {
-    props.setShowArticleModal(true);
-    props.setShowModal(false);
-  };
+
+
+  // const showArticleModalHandler = () => {
+  //   props.setShowArticleModal(true);
+  //   props.setShowModal(false);
+  // };
 
   return (
     <div className="backdrop">
+      <input
+        id="upload-image"
+        type="file"
+        style={{ display: "none" }}
+        accept="image/jpg,impge/png,image/jpeg"
+        name="profile_img"
+        onChange={changePhotoHandler}
+        ref={imageInput}
+      />
       <div className="modalContainer">
         <div className="titleContainer">
           <h2 className="title">추억을 기록하세요</h2>
@@ -42,9 +69,9 @@ const PhotoInputModal: React.FC<{
         <div className="btnContainer">
           <button
             className="bg-blue-400 mx-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={showArticleModalHandler}
+            onClick={uploadImageHandler}
           >
-            게시글 작성
+            사진 등록
           </button>
           <button className="bg-red-100 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             사진 삭제
