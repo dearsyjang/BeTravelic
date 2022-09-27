429f4f6d43107fbd6d12ca3945bd50ea67bdb54a
조호형 <jhh6822@naver.com>
2022-09-27 09:21:25 +0900

    [feat] 마이페이지 댓글 생성 삭제

--- Back/demo/src/main/java/beTravelic/demo/domain/controller/CommentController.java
+++ Back/demo/src/main/java/beTravelic/demo/domain/controller/CommentController.java
@@ -2,6 +2,7 @@ package beTravelic.demo.domain.controller;
 
 
 import beTravelic.demo.domain.dto.CommentSaveRequestDto;
+import beTravelic.demo.domain.dto.CommentUpdateRequestDto;
 import beTravelic.demo.domain.entity.Comment;
 import beTravelic.demo.domain.service.CommentService;
 import beTravelic.demo.global.common.CommonResponse;
@@ -15,21 +16,35 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("sns/{review_id}/comment")
 public class CommentController {
 
-//    private final CommentService commentService;
-//    @PostMapping
-//    public ResponseEntity<CommonResponse> commentSave(@RequestParam("id") String id, @RequestParam("review_id") Long review_id, CommentSaveRequestDto dto){
-//        return new ResponseEntity<>(CommonResponse.getSuccessResponse(commentService.commentSave(id, review_id, dto)), HttpStatus.OK);
-//    }
+    private final CommentService commentService;
+    @PostMapping
+    public ResponseEntity<CommonResponse> commentSave(@RequestParam("id") String id, @RequestParam("review_id") Long review_id, CommentSaveRequestDto dto){
+        return new ResponseEntity<>(CommonResponse.getSuccessResponse(commentService.commentSave(id, review_id, dto)), HttpStatus.OK);
+    }
 
 //    @PutMapping("/{comment_id}")
-//    public ResponseEntity<CommonResponse> commentUpdate(@RequestParam("id") String id, @RequestParam("comment_id") Long comment_id, CommentSaveRequestDto dto){
-//        return new ResponseEntity<>(CommonResponse.getSuccessResponse(commentService.commetUpdate(id, comment_id, dto)), HttpStatus.OK);
+//    public ResponseEntity<?> commentUpdate(@RequestParam("id") String id, @PathVariable("comment_id") Long comment_id, CommentUpdateRequestDto dto) {
+////        return new ResponseEntity<>(CommonResponse.getSuccessResponse(commentService.commentUpdate(id, comment_id, dto)), HttpStatus.OK);
+//        try {
+//            commentService.commentUpdate(id, comment_id, dto);
+//            return new ResponseEntity<>(true, HttpStatus.valueOf(201));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return new ResponseEntity<>(false, HttpStatus.valueOf(500));
+//        }
 //    }
 //
-//    @DeleteMapping("/{comment_id}")
-//    public ResponseEntity<CommonResponse> commentUpdate(@RequestParam("id") String id, @RequestParam("comment_id") Long comment_id){
+    @DeleteMapping("/{comment_id}")
+    public ResponseEntity<?> commentDelete(@RequestParam("id") String id, @RequestParam("comment_id") Long comment_id){
+        try {
+            commentService.commentDelete(id, comment_id);
+            return new ResponseEntity<>(true, HttpStatus.valueOf(200));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.valueOf(400));
+        }
 //        return new ResponseEntity<>(CommonResponse.getSuccessResponse(commentService.commentDelete(id, comment_id)), HttpStatus.OK);
-//    }
+    }
 
 
 
