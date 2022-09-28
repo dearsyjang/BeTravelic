183f22fcb519adf7bcb0604df389fcdc90870d62
Johohyeong <jhh6822@naver.com>
2022-09-28 16:22:29 +0900

    [feat] 리뷰의 댓글 리스트 조회

--- Back/demo/src/main/java/beTravelic/demo/domain/controller/CommentController.java
+++ Back/demo/src/main/java/beTravelic/demo/domain/controller/CommentController.java
@@ -1,25 +1,44 @@
 package beTravelic.demo.domain.controller;
 
 
+import beTravelic.demo.domain.dto.CommentResDto;
 import beTravelic.demo.domain.dto.CommentSaveRequestDto;
 import beTravelic.demo.domain.dto.CommentUpdateRequestDto;
+import beTravelic.demo.domain.dto.ReviewResDto;
 import beTravelic.demo.domain.entity.Comment;
 import beTravelic.demo.domain.service.CommentService;
 import beTravelic.demo.global.common.CommonResponse;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("sns/{review_id}/comment")
+@RequestMapping("/feed/travel-reveiw/{reviewId}/comment")
 public class CommentController {
 
     private final CommentService commentService;
+    @ApiOperation(value = "리뷰에 댓글 등록", notes = "성공 시 리뷰, 실패 시 null")
     @PostMapping
-    public ResponseEntity<CommonResponse> commentSave(@RequestParam("id") String id, @RequestParam("review_id") Long review_id, CommentSaveRequestDto dto){
+    public ResponseEntity<CommonResponse> commentSave(@RequestParam("id") String id, @RequestParam("reviewId") Long review_id, CommentSaveRequestDto dto){
         return new ResponseEntity<>(CommonResponse.getSuccessResponse(commentService.commentSave(id, review_id, dto)), HttpStatus.OK);
+    }
+
+
+    @ApiOperation(value = "리뷰의 댓글 조회", notes = "성공 시 리뷰, 실패 시 null")
+    @GetMapping("")
+    public ResponseEntity<?> getCommentByReview(HttpServletRequest request, @PathVariable("reviewId") Long reviewId){
+        try {
+            List<CommentResDto> comments = commentService.findAllByReview(reviewId);
+            return ResponseEntity.ok(comments);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
 //    @PutMapping("/{comment_id}")
@@ -34,8 +53,9 @@ public class CommentController {
 //        }
 //    }
 //
-    @DeleteMapping("/{comment_id}")
-    public ResponseEntity<?> commentDelete(@RequestParam("id") String id, @RequestParam("comment_id") Long comment_id){
+    @ApiOperation(value = "리뷰에 댓글 삭제", notes = "성공 시 true, 실패 시 false")
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<?> commentDelete(@RequestParam("id") String id, @RequestParam("commentId") Long comment_id){
         try {
             commentService.commentDelete(id, comment_id);
             return new ResponseEntity<>(true, HttpStatus.valueOf(200));
@@ -43,7 +63,6 @@ public class CommentController {
             e.printStackTrace();
             return new ResponseEntity<>(false, HttpStatus.valueOf(400));
         }
-//        return new ResponseEntity<>(CommonResponse.getSuccessResponse(commentService.commentDelete(id, comment_id)), HttpStatus.OK);
     }
 
 
