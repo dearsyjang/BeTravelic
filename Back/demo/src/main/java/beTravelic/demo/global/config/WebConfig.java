0c8cb1a181f8ee322e3e0d7c17371fc255e2c91d
정윤해 <ue0828@naver.com>
2022-09-29 23:59:02 +0900

    Develop

--- Back/demo/src/main/java/beTravelic/demo/global/config/WebConfig.java
+++ Back/demo/src/main/java/beTravelic/demo/global/config/WebConfig.java
@@ -5,6 +5,7 @@ import beTravelic.demo.global.util.jwt.RefreshTokenInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,18 +17,24 @@ public class WebConfig implements WebMvcConfigurer {
     private final AccessTokenInterceptor accessTokenInterceptor;
     private final RefreshTokenInterceptor refreshTokenInterceptor;
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET","POST","PUT","DELETE", "OPTIONS");
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                );
+    }
 
 //    @Override
 //    public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(refreshTokenInterceptor).addPathPatterns("/users/access-token");
 //        registry.addInterceptor(accessTokenInterceptor).excludePathPatterns(Arrays.asList(
-//                new String[]{"/users/token", "/users/id/{id}", "/users/nickname/{nickname}", "/users/id", "/users/password", "/h2-console", "/swagger-ui/*", "/swagger-resources", "/swagger-resources/**", "/v3/*", "/v3", "/users/{userId}/image"}));
+//                new String[]{"/users", "/users/login", "/swagger-ui.html", "/swagger-ui.html/**", "/swagger-resources", "/swagger-resources/**", "/v3/*", "/v3"}));
 //    }
 
 }
