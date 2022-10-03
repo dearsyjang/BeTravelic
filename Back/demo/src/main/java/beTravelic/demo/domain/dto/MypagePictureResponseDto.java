9172cd51e3bb7910bec792b2364718c38a8379d6
정윤해 <ue0828@naver.com>
2022-10-03 18:47:35 +0900

    Develop

--- Back/demo/src/main/java/beTravelic/demo/domain/dto/MypagePictureResponseDto.java
+++ Back/demo/src/main/java/beTravelic/demo/domain/dto/MypagePictureResponseDto.java
@@ -1,11 +1,23 @@
-//package beTravelic.demo.domain.dto;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//
-//@Data
-//@AllArgsConstructor
-//public class MypagePictureResponseDto {
-//    private String image;
-//    private String region;
-//}
+package beTravelic.demo.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Column;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class MypagePictureResponseDto {
+    private Long user_id;
+    private String region;
+    private String realFileName;
+    public static MypagePictureResponseDto of(String region, String realFileName, Long user_id){
+        return  MypagePictureResponseDto.builder()
+                .realFileName(realFileName)
+                .region(region)
+                .user_id(user_id)
+                .build();
+    }
+}
