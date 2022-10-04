72cb9a30d953b64bd1e4cc32962d14cdaca8139d
정윤해 <ue0828@naver.com>
2022-10-04 16:16:08 +0900

    Revert "Merge branch 'feature/data/final/S07P22D205-230' into 'develop'"

--- Back/demo/src/main/java/beTravelic/demo/domain/service/PictureService.java
+++ Back/demo/src/main/java/beTravelic/demo/domain/service/PictureService.java
@@ -40,21 +40,6 @@ public class PictureService {
     public String uploadFileToGCS(String id, MultipartFile proFile) throws IOException {
 
 
-//        File path = new File("");
-//        System.out.println(path.getAbsolutePath());
-
-//        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream(path.getAbsolutePath() + "\\src\\main\\resources\\static/civil-forge-364402-29986bfb28c2.json"));
-//        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("http://localhost:8443/civil-forge-364402-29986bfb28c2.json"));
-
-//        String keyFileName = "";
-//        InputStream keyFile = ResourceUtils.getURL(path.getAbsolutePath() + "\\src\\main\\resources\\static/civil-forge-364402-29986bfb28c2.json").openStream();
-//        InputStream keyFile = ResourceUtils.getURL(path.getAbsolutePath() + "\\src\\main\\resources\\static/civil-forge-364402-29986bfb28c2.json").openStream();
-
-
-//        Storage storage = StorageOptions.newBuilder().setProjectId("BeTravelic")
-//                // Key 파일 수동 등록
-//                .setCredentials(credentials)
-//                .build().getService();
         File path = new File("");
         System.out.println(path.getAbsolutePath());
 
@@ -107,5 +92,56 @@ public class PictureService {
         String profileUrl = "https://storage.googleapis.com/be_travelic/" + user.getPicture().getRealFileName();
         return profileUrl;
     }
+    public void updateFileToGCS(String id, MultipartFile proFile) throws IOException {
 
+
+        File path = new File("");
+        System.out.println(path.getAbsolutePath());
+
+        InputStream keyFile = ResourceUtils.getURL("classpath:static/civil-forge-364402-29986bfb28c2.json").openStream();
+
+        Storage storage = StorageOptions.newBuilder().setProjectId("BeTravelic")
+                // Key 파일 수동 등록
+                .setCredentials(GoogleCredentials.fromStream(keyFile))
+                .build().getService();
+
+
+        Picture picture = null;
+        User user = userRepository.findUserById(id).orElseThrow(() ->
+                new RuntimeException("일치하는 사용자 없음"));
+        File oldFile = new File(IMAGE_PATH + user.getPicture().getRealFileName());
+        oldFile.delete();
+
+        String fileName = UUID.randomUUID().toString();
+        String contentType = proFile.getContentType();
+        File file = null;
+        if(contentType.contains("image/jpeg")){
+            file = new File(IMAGE_PATH + fileName + ".jpg");
+            picture = Picture.builder().fileName(fileName).realFileName(fileName + ".jpg").build();
+        }else if(contentType.contains("image/png")){
+            file = new File(IMAGE_PATH + fileName + ".png");
+            picture = Picture.builder().fileName(fileName).realFileName(fileName + ".png").build();
+        }else if(contentType.contains("image/gif")){
+            file = new File(IMAGE_PATH + fileName + ".gif");
+            picture = Picture.builder().fileName(fileName).realFileName(fileName + ".gif").build();
+        }else{
+            new RuntimeException("지원하는 사진 형식이 아닙니다");
+        }
+
+        File convertFile = convertMultiPartToFile( proFile );
+
+//        proFile.transferTo(file);
+        user.setProfile(picture);
+        userRepository.save(user);
+
+        BlobInfo blobInfo = storage.create(
+                BlobInfo.newBuilder("be_travelic", picture.getRealFileName())
+                        .setAcl(new ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))))
+                        .build());
+
+        Blob blob = storage.createFrom(blobInfo, new FileInputStream(convertFile));
+
+//        return blob;
+
+    }
 }
