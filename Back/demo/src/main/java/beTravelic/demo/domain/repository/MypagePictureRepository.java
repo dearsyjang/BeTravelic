4e44d6194b7bc6b664764136cc543dc753f20874
정윤해 <ue0828@naver.com>
2022-10-04 16:02:26 +0900

    Resolve S07P22D205-201 "Feature/be/user/image/"

--- Back/demo/src/main/java/beTravelic/demo/domain/repository/MypagePictureRepository.java
+++ Back/demo/src/main/java/beTravelic/demo/domain/repository/MypagePictureRepository.java
@@ -1,13 +1,21 @@
-//package beTravelic.demo.domain.repository;
-//
-//import beTravelic.demo.domain.entity.MypagePicture;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface MypagePictureRepository extends JpaRepository<MypagePicture, Long> {
-//
-//
-//
-//    Optional<MypagePicture> findMypageByUser(String id);
-//}
+package beTravelic.demo.domain.repository;
+
+import beTravelic.demo.domain.entity.MypagePicture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MypagePictureRepository extends JpaRepository<MypagePicture, Long> {
+    @Autowired
+    Optional<MypagePicture> findMypageByUserId(String id);
+
+    Optional<MypagePicture> findByUserAndRegion(String id, Long region_id);
+//    List<MypagePicture>findMypageListByUserId(String id);
+
+}
