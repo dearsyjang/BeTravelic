e4974c2ca3ab680e6ee95c49616591ce9be79eaf
Johohyeong <jhh6822@naver.com>
2022-10-06 17:26:08 +0900

    [fix] follower & following List 수정

--- Back/demo/src/main/java/beTravelic/demo/domain/dto/FollowerListResponseDto.java
+++ Back/demo/src/main/java/beTravelic/demo/domain/dto/FollowerListResponseDto.java
@@ -7,14 +7,15 @@ import lombok.Data;
 @Builder
 @Data
 public class FollowerListResponseDto {
-
+    private Long user_id;
     private String follower_id;
     private String nickname;
 
     public static FollowerListResponseDto of(Follow follow){
         return FollowerListResponseDto.builder()
-                .follower_id(follow.getFollower().getId())
-                .nickname(follow.getFollower().getNickname())
+                .user_id(follow.getFollowing().getUser_id())
+                .follower_id(follow.getFollowing().getId())
+                .nickname(follow.getFollowing().getNickname())
                 .build();
     }
 }
