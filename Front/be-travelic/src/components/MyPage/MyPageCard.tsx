a1c91b0f4cfd23f0a70c484d07a5d6ac7cf6352b
신혜원 <hehe2you@gmail.com>
2022-10-06 20:09:33 +0900

    [fix] : frequentplace 삭제

--- Front/be-travelic/src/components/MyPage/MyPageCard.tsx
+++ Front/be-travelic/src/components/MyPage/MyPageCard.tsx
@@ -8,6 +8,7 @@ import { fetchUserInfo, userInfoType } from "../../apis/mypage";
 import { userInfo } from "os";
 import KakaoShare from "../common/KakaoShare";
 import { useParams } from "react-router-dom";
+import myhome from "../../assets/image/myhome.png";
 
 const MyPageCard = () => {
   const { id } = useParams();
@@ -18,13 +19,20 @@ const MyPageCard = () => {
     surveyKeyword: [],
     reviewCnt: 0,
     user_id: Number(id),
+    nickname: "",
   });
 
   useLayoutEffect(() => {
     const initialData = async () => {
       const res = await fetchUserInfo(userInfos.user_id);
-      const { followerCnt, followingCnt, reviewCnt, surveyKeyword, user_id } =
-        res;
+      const {
+        followerCnt,
+        followingCnt,
+        reviewCnt,
+        surveyKeyword,
+        user_id,
+        nickname,
+      } = res;
       console.log(res, "여기는 마이페이지 카드");
 
       setUserInfo({
@@ -33,13 +41,19 @@ const MyPageCard = () => {
         reviewCnt,
         surveyKeyword,
         user_id,
+        nickname,
       });
     };
     initialData();
   }, []);
 
   return (
-    <div className="cardContainer" id="">
+    <div className="cardContainer">
+      <div className="flex justify-center font-bold text-xl m-10 text-blue-400">
+        <img src={myhome} className='w-7 mr-2' />
+        {userInfos.nickname}
+        <p className="font-light text-sm text-dark-gray m-1">님의 마이페이지</p>
+      </div>
       <div>
         <UploadPhoto
           userId={userInfos.user_id}
@@ -53,9 +67,6 @@ const MyPageCard = () => {
       </div>
       <div className="mt-5">
         <TripStyle tripStyles={userInfos.surveyKeyword} />
-      </div>
-      <div className="mt-5">
-        <FrequentPlace />
       </div>
     </div>
   );
